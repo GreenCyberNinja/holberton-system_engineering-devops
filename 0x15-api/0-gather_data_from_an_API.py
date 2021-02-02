56275d79df672b11ee4dc89eaec9da0ab2c25035
GreenCyberNinja <1824@holbertonschool.com>
2021-02-02 03:47:53 +0000

    readme and first file

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""gets from api and returns information about his/her TODO list progress"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """displays completed tasks"""
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0 
+    TASKS = []
+    empid = argv[1]
+    empurl = 'https://jsonplaceholder.typicode.com/users/{}'\
+        .format(int(empid))
+    EMPLOYEE_NAME = requests.get(empurl).json()
+    print("Employee {} is done with tasks"
+          .format(EMPLOYEE_NAME.get('name')), end="")
+    todourl = ("https://jsonplaceholder.typicode.com/todos")
+    urlreq = requests.get(todourl).json()
+    for toto in urlreq:
+        if toto.get('userId') == int(empid):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if toto.get('completed') is TRUE:
+                NUMBER_OF_DONE_TASKS += 1
+                TASKS.append(toto.get("title"))
+    print('{}/{}):'.format(NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for TASK_TITLE in TASKS:
+        print('\t {}'.format(TASK_TITLE))
+

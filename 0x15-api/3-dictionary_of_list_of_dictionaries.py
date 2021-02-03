efe871ba6660de4d78f93abc2685bd5679a21249
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-02-03 10:47:47 +0000

    dictionary to find meaning

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""gets from api and returns information about his/her TODO list progress"""
+
+from requests import get
+import json
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    usernames = get(url + '/users/').json()
+    userdict = {}
+    for user in usernames:
+        username = get(url + '/users/' + str(user["id"])).json().get('username')
+        tasks = get(url + '/users/' + str(user["id"]) +'/todos').json()
+        tasklist = []
+        for task in tasks:
+            taskdict = {}
+            taskdict["task"] = task.get('title')
+            taskdict["completed"] = task.get('completed')
+            taskdict["username"] = "{}".format(username)
+            tasklist.append(taskdict)
+        userdict[str(user["id"])] = tasklist
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        jsonfile.write(json.dumps(userdict))

c89d3420f9321997ca26665c33c19d2751f0f163
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-02-04 23:53:32 +0000

    fixed err statement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,13 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'user'}
-    Rdict = get(url, headers=headers).json()
+    headers = {'user-agent': 'user',}
+    try:
+        Rdict = get(url, headers=headers, allow_redirects=False).json()
+    except(ValueError):
+        return 0;
     data = Rdict.get('data')
-    return (data.get('subscribers'))
+    sub = data.get('subscribers')
+    if sub == None:
+        sub = 0;
+    return sub

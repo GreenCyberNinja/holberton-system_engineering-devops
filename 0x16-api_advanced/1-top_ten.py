d1ba3a15e5388803606e73153c5f0e01ecd4a5bd
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-02-05 02:50:11 +0000

    answers for task 1 and 2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""gets the subscribers from reddit api"""
+from requests import get
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot/.json?count=1".format(subreddit)
+    headers = {'user-agent': 'user'}
+    Rdict = get(url, headers=headers, allow_redirects=False)
+    if Rdict.status_code is not 200:
+        print('None')
+    else:
+        data = Rdict.json().get('data').get('children')
+        count = 0
+        while count < 10:
+            subdata = data[count].get('data').get('title')
+            print(subdata)
+            count += 1	
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""gets the subscribers from reddit api"""
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {'user-agent': 'user'}
+    Rdict = get(url, headers=headers, params={'after': after}, allow_redirects=False)
+    if Rdict.status_code is not 200:
+        return('None')
+    else:
+        data = Rdict.json().get('data').get('children')
+        for title in data:
+            hot_list.append(title.get('data').get('title'))
+        after = Rdict.json().get('data').get('after')
+        if after:
+            recurse(subreddit, hot_list, after)
+        return hot_list

2332b03ed36bbc8e4e19d12b6178e0b7c71b6897
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-02-25 23:28:06 +0000

    puppet script updates the config file to prevent failed request

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# fix the config to prevent failed request
+exec {'max files setting':
+   command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
+}

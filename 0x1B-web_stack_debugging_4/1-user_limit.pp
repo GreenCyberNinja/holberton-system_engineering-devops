775ac4f57f16705fc30f6818b65d98a6a27fb5d6
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-02-28 05:59:36 +0000

    task 1 fixed config:

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# debugging task #1
+exec { 'fix_hardlimit':
+  path    => ['/usr/bin/', '/bin/'],
+  command => 'sed -i "56c holberton hard nofile 1048576" /etc/security/limits.conf',
+}
+exec { 'fix_softlimit':
+  path    => ['/usr/bin/', '/bin/'],
+  command => 'sed -i "57c holberton soft nofile 1048576" /etc/security/limits.conf',
+}
